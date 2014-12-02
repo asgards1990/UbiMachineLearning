4972c51d80ddf4f8e01f947551c878637088bb6b
unknown <su.yang@SFO-LAP-RD2601.ubisoft.org>
2014-12-01 17:07:00 -0800

    21 features 100k data, leaver is defined as someone hasn't played for 60 days dating back from getdate() and has level 2 minimum, the profiles are retrive as the snapshot of the player at least 6 days before the last played date

--- UbiLeaverPrediction.py
+++ UbiLeaverPrediction.py
@@ -6,9 +6,7 @@ Created on Sat Nov 29 09:34:47 2014
 """
 
 import numpy as np
-#import pylab as pl
 import pandas as pd
-import csv
 from sklearn.ensemble import RandomForestClassifier
 classifier=RandomForestClassifier(n_estimators=1000, 
                        max_depth=100,
@@ -17,41 +15,38 @@ classifier=RandomForestClassifier(n_estimators=1000,
                        oob_score=True,
                        verbose=0)
 
-#Having the raw data needed                   
-file_location="C:/Users/corpus/Desktop/UbiMachineLearning/Ubi100KLeaverFor7Days3DaysBufferOutcomeInversed.csv"
-df = pd.DataFrame.from_csv(file_location, sep=';')
+#Retrieving the raw data               
+file_location="C:/Users/su.yang/Desktop/UbiMachineLearning/Ubi100KFileLeaverFor60Days1WeekBufferFromLevel2.csv"
+df = pd.DataFrame.from_csv(file_location, sep=',')
+
+
+#cleaning data
 totalLength = len(df)
 index = np.arange(totalLength)
 df.index = index
-
-#cleaning data
-df.index = index
 del df['Ip']
-del df['FirstPayDt']
-del df['FirstPlayedDt']
 del df['CountryShort']
-del df['MaxDayMinusThree']
-del df['totalUSDMaxDayMinusThree']
-del df['USDMaxDayMinusThree']
 for i in range (totalLength):
     if pd.isnull(df['paygap'][i]):
         df['paygap'][i] =0
+    if pd.isnull(df['daysFrom20140718ToFirstPayDt'][i]):
+        df['daysFrom20140718ToFirstPayDt'][i]=0
 
 #randomized train and test samples.                   
 df['is_train'] = np.random.uniform(0, 1, totalLength) <= .75
 train, test = df[df['is_train']==True], df[df['is_train']==False]
 
 #names of the features
-features = df.columns[:16]
+features = df.columns[:21]
 
 #training
-y, _ = pd.factorize(train['stopPlayingBeforeTheDay'])
+y, _ = pd.factorize(train['stopPlayingBeforeTheDayAtLeastLevel2'])
 classifier.fit(train[features],y)
 #prediction on test set
 preds = classifier.predict(test[features])
 
 #crosstabChecking 
-pd.crosstab(test['stopPlayingBeforeTheDay'], preds, rownames=['actual'], colnames=['preds'])
+pd.crosstab(test['stopPlayingBeforeTheDayAtLeastLevel2'], preds, rownames=['actual'], colnames=['preds'])
 
 
 from sklearn.metrics import precision_recall_curve
@@ -62,10 +57,13 @@ precision = dict()
 recall = dict()
 average_precision = dict()
 
-prc = precision_recall_curve(test['stopPlayingBeforeTheDay'],
+prc = precision_recall_curve(test['stopPlayingBeforeTheDayAtLeastLevel2'],
                                                         preds)
-average_precision = average_precision_score(test['stopPlayingBeforeTheDay'],
+average_precision = average_precision_score(test['stopPlayingBeforeTheDayAtLeastLevel2'],
                                                         preds)
                                                         
-test = precision_recall_fscore_support(test['stopPlayingBeforeTheDay'],
+#they are in array because it computes precision and recall in relation to 0 
+#and 1 respectively. Teh last/fourth array is the number of occurences in the
+#actual data.                                                        
+fscore = precision_recall_fscore_support(test['stopPlayingBeforeTheDayAtLeastLevel2'],
                                                         preds)                                                        
